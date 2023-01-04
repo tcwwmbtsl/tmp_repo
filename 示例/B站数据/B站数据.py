115466831d713ccd6d2df4495ea79e01dec53fe9
lwx1039194 <flfusrusr@163.com>
2023-01-05 00:12:56 +0800

    代码推送

--- 示例/B站数据/B站数据.py
+++ 示例/B站数据/B站数据.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+
 import requests
 from pathlib import Path
 url = 'https://api.bilibili.com/x/web-interface/wbi/search/type?__refresh__=true&_extra=&context=&page=1&page_size=42&from_source=&from_spmid=333.337&platform=pc&highlight=1&single_column=0&keyword=%E7%BE%8E%E5%A5%B3&qv_id=qy7Vk2tYduoFh1J5tLnVegaMjCMGuzFN&ad_resource=5654&source_tag=3&category_id=&search_type=video&dynamic_offset=0&w_rid=0b9735625dadc9b2f3f3d839d1d5fc01&wts=1672678143'
