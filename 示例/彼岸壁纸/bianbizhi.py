5234f599cde83c3b58e0a1e72e5827c343177974
lwx1039194 <flfusrusr@163.com>
2023-01-10 01:19:31 +0800

    示例成功

--- 示例/彼岸壁纸/bianbizhi.py
+++ 示例/彼岸壁纸/bianbizhi.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 import requests
+from pathlib import Path
 import re
 
 url = 'http://www.netbian.com/index_2.htm'
@@ -12,8 +13,18 @@ headers = {
 }
 response = requests.get(url, headers=headers)
 response.encoding = response.apparent_encoding
-print(response.text)
-pattern = '<a href="/desk/29145.htm" title="(.*?) 更新时间.*?<<img src="(.*?)"'
+pattern = '<a href=".*?" title="(.*?)".*?<img src="(.*?)"'
 # <a href="/desk/29145.htm" title="原创 兔年吉祥 2023年1月日历桌面壁纸 更新时间：2023-01-04" target="_blank"><img src="http://img.netbian.com/file/2022/1213/small233252tEQi81670945572.jpg" alt="原创 兔年吉祥 2023年1月日历桌面壁纸"><b>原创 兔年吉祥 2023年1月日历桌面壁纸</b></a>
 li_list = re.findall(pattern, response.text)
-print(li_list)
+print(len(li_list))
+current_path = Path(__file__).parent 
+dir_path = Path(current_path, 'pic')
+if not Path.is_dir(dir_path):
+    Path.mkdir(dir_path)
+
+for title, url in li_list:
+    file_path = Path(dir_path, title.replace(' ', '')[0:5]+'.jpg')
+
+    content = requests.get(url, headers=headers).content
+    with open(file_path, 'wb') as f:
+        f.write(content)
